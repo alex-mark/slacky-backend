be76cc5e2dd3663f8a10e175f20180f33d4861b6
alex-mark <alexndr86@gmail.com>
2018-02-10 14:50:23 +0300

    createChannel update

--- schema/channel.js
+++ schema/channel.js
@@ -8,11 +8,17 @@ export default `
     users: [User!]!
   }
 
+  type ChannelResponse {
+    ok: Boolean!
+    channel: Channel
+    errors: [Error!]
+  }
+
   type Mutation {
     createChannel(
       teamId: Int!,
       name: String!,
       public: Boolean=false
-    ): Boolean!,
+    ): ChannelResponse,
   }
 `;
