fae7a786decc2df360f4a850bf2f8ea2a35e3631
alex-mark <alexndr86@gmail.com>
2018-02-11 22:21:03 +0300

    createTeam transaction

--- resolvers/team.js
+++ resolvers/team.js
@@ -9,17 +9,20 @@ export default {
   Mutation: {
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const team = await models.Team.create({ ...args, owner: user.id });
-        await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+        const response = await models.sequelize.transaction(async () => {
+          const team = await models.Team.create({ ...args, owner: user.id });
+          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+          return team;
+        });
         return {
           ok: true,
-          team,
+          team: response,
         };
       } catch (err) {
         console.log(err);
         return {
           ok: false,
-          errors: formatErrors(err),
+          errors: formatErrors(err, models),
         };
       }
     }),
@@ -41,12 +44,14 @@ export default {
           };
         }
         await models.Member.create({ userId: userToAdd.id, teamId });
-        return { ok: true };
+        return {
+          ok: true,
+        };
       } catch (err) {
         console.log(err);
         return {
           ok: false,
-          errors: formatErrors(err),
+          errors: formatErrors(err, models),
         };
       }
     }),
